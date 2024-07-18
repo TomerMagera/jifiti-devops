7bf4bdc8ff4c7927fa4a30f7b23eb7c14be2bad9
Tomer Magera <tomer_magera@webiks.com>
2024-07-18 16:49:37 +0300

    terraform code for aws initialization - s3 bucket for the state maintanance.

---
+++ terraform/aws-tf-init/variables.tf
@@ -0,0 +1,13 @@
+variable "env" {
+  type    = string
+  default = "jifiti-devops"
+}
+variable "aws_profile" {
+  type    = string
+  default = "jifiti-devops"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
